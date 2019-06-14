c603ac54acc06c603c6ba488e7ce0b97d755ff3e
caixiwen <williamcai663@gmail.com>
2019-06-14 23:51:14 +0800

    add aspect

--- src/main/java/com/cxw/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/cxw/springbootdemo/SpringBootDemoApplication.java
@@ -3,6 +3,7 @@ package com.cxw.springbootdemo;
 import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
@@ -11,7 +12,7 @@ import javax.jms.Queue;
 
 @SpringBootApplication
 @EnableCaching
-public class SpringBootDemoApplication {
+public class SpringBootDemoApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
@@ -22,4 +23,6 @@ public class SpringBootDemoApplication {
     public Queue queue() {
        return new ActiveMQQueue("active.queue");
     }
+
+
 }
---
+++ src/main/java/com/cxw/springbootdemo/infra/OperateLogAspect.java
@@ -0,0 +1,7 @@
+package com.cxw.springbootdemo.infra;
+
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class OperateLogAspect {
+}
